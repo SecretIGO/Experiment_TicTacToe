fcf22ed38abab5707c862983c49cc3aa6b64141b
SecretIGO <110442934+SecretIGO@users.noreply.github.com>
2024-01-08 17:52:18 +0800

    Game logic. no proper ui, yet.
    Placed all functions in their proper classes!

--- app/src/main/java/com/experiments/experimenttictactoe/MainGameActivity.kt
+++ app/src/main/java/com/experiments/experimenttictactoe/MainGameActivity.kt
@@ -14,49 +14,20 @@ import com.experiments.experimenttictactoe.ticTacToe.GameLogic
 
 class MainGameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainGameBinding
-    private val boardLogic = BoardLogic()
     private val gameLogic = GameLogic()
 
-    private var player = 1
-    private var numRows = 3
-    private var numColumns = 3
-    private var board = Array(numRows) { Array(numColumns) { 0 } }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        for (row in 0 until numRows)
-            for (col in 0 until numColumns) {
-                val square = boardLogic.createBoard(this, row, col) {
-                    if (!gameLogic.isGameOver()){
-                        board[row][col] = player
-                        gameLogic.checkPlayerVictory(board, player)
-                        player = gameLogic.handleSquareSelected(player, it.getChildAt(0) as TextView)
-
-                        Log.d("BoardObserver", "Current board state:\n${boardToString()}")
-                    }
-                    else {
-                        Log.d("BoardObserver", "Game is Over!!")
-                    }
-                }
-                binding.gridLayout.addView(square)
-            }
+        gameLogic.playGame(this@MainGameActivity, binding)
 
         gameLogic.gameOverLiveData.observe(this, Observer { gameOver ->
             if (gameOver) {
-                Toast.makeText(this, "Player$player victory!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Player${gameLogic.player} victory!", Toast.LENGTH_SHORT).show()
             }
         })
-    }
-
-    private fun boardToString(): String {
-        val builder = StringBuilder()
-        for (row in board) {
-            builder.append(row.joinToString(" "))
-            builder.append("\n")
-        }
-        return builder.toString()
     }
 
     companion object {
